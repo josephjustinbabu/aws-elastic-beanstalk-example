bc0be352fed57623d0bc01988752c75d2eab3741
Joseph Justin <justin.joseph@pearson.com>
2021-10-05 11:36:18 +0530

    Intial Commit

---
+++ src/main/java/com/beanstalk/example/AwsElasticBeanstalkExampleApplication.java
@@ -0,0 +1,12 @@
+package com.beanstalk.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstalkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstalkExampleApplication.class, args);
+	}
+}
